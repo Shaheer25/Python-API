fcc69cacb78e57bb08f783e7599c66ac8e0e9cd5
Shaheer25 <shaheer@logichive.in>
2023-08-28 19:03:17 +0530

    Python

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from bson.objectid import ObjectId
 from flask_jwt_extended import JWTManager, create_access_token, decode_token, jwt_required, get_jwt_identity
 import bcrypt
 from datetime import timedelta
+
+
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "jhfshgfghsfags"
 jwt = JWTManager(app)
@@ -20,7 +22,7 @@ except:
     print("Error - Connecting to DB")
 
 @app.route('/users', methods =['POST'])
-def createUser():  # put application's code here
+def create():  # put application's code here
     try:
         user={
             "name":request.json["name"],
