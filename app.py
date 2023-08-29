2cad9bd9d104532777897f7609e19e02d151eea0
Shaheer25 <shaheer@logichive.in>
2023-08-29 12:34:56 +0530

    che

--- app.py
+++ app.py
@@ -6,179 +6,238 @@ from flask_jwt_extended import JWTManager, create_access_token, decode_token, jw
 import bcrypt
 from datetime import timedelta
 
-
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = "jhfshgfghsfags"
 jwt = JWTManager(app)
-try:
-    mongo = pymongo.MongoClient(
+app.config["JWT_SECRET_KEY"] = "jhfshgfghsfags"
+try :
+    mongo=pymongo.MongoClient(
         host="localhost",
         port=27017,
-        serverSelectionTimeoutMS=1000
     )
-    db = mongo.crud
+    db=mongo.test
     mongo.server_info()
-except:
-    print("Error - Connecting to DB")
+except :
+    print("Error Connecting to DB")
 
-@app.route('/users', methods =['POST'])
-def create():  # put application's code here
+@app.route("/users",methods=["POST"])
+
+def create():
     try:
-        user={
-            "name":request.json["name"],
-            "dob":request.json["dob"]
+        users = {
+            "firstName":request.json["firstName"],
+            "lastName":request.json["lastName"],
+            "email":request.json["email"],
+            "phone":request.json["phone"],
+            "userType":request.json["userType"]
         }
-        dbResponse = db.users.insert_one(user)
-        # print(dbResponse.inserted_id)
-        return Response (
-            response=json.dumps(
-                {"message":"User Created",
-                 "id":f"{dbResponse.inserted_id}"
-                  }),
+
+        dbResponse = db.users.insert_one(users)
+
+        return Response(
+            response=json.dumps({
+                "message":"User Added Successfully",
+                "id":f"{dbResponse.inserted_id}"
+            }),
             status=200,
             mimetype="application/json"
         )
-        # for attribute in dir(dbResponse):
-        #     print(attribute)
-    except Exception as ex:
-        print(ex)
+    except Exception as exp:
+        print(exp)
 
-@app.route("/users", methods =["GET"])
+@app.route("/users",methods=["GET"])
 def get():
     try:
         data = list(db.users.find())
         for user in data :
-            user["_id"]= str(user["_id"])
-
-        return Response(
+            user["_id"]=str(user["_id"])
+        return Response (
             response=json.dumps(data),
             status=200,
             mimetype="application/json"
         )
-    except Exception as ex:
-        print(ex)
-        return Response (
-            response=json.dumps(
-                {"message": "Cannot read Users",}),
-            status=500,
-            mimetype="application/json"
-        )
-@app.route("/users/<id>", methods=["PUT"])
+    except Exception as exp:
+        print(exp)
+    return Response(
+        response=json.dumps({
+            "message":"error while fetching user"
+        }),
+        status = 500,
+        mimetype="application/json"
+    )
+
+@app.route("/users/<id>",methods=["PUT"])
 def update(id):
-    try :
-        update_fields = {}
+     try:
+        fields={}
 
-        if "name" in request.json:
-            update_fields["name"] = request.json["name"]
+        if "firstName" in request.json:
+            fields["firstName"] = request.json["firstName"]
 
-        if "dob" in request.json:
-            update_fields["dob"] = request.json["dob"]
+        if "email" in request.json:
+            fields["email"] = request.json["email"]
+
+        if "LastName" in request.json:
+            fields["LastName"] = request.json["LastName"]
+
+        if "phone" in request.json:
+            fields["phone"] = request.json["phone"]
 
         dbResponse = db.users.update_one(
             {"_id": ObjectId(id)},
-            {"$set": update_fields}
+            {"$set": fields}
         )
-
-        # for attr in dir(dbResponse):
-        #     print(f"{attr}")
-        if dbResponse.modified_count==1:
+        if dbResponse.modified_count == 1:
+            return Response(
+            response=json.dumps(
+                {"message":"Updated Successfully"}
+            ),
+            status=200,
+                mimetype="application/json"
+        )
+        else:
             return Response(
                 response=json.dumps(
-                    {"message": "User Updated Successfully", }),
+                    {"message": "Nothing to Update"}
+                ),
+                status=200,
+                mimetype="application/json"
+            )
+     except Exception as exp:
+         print(exp)
+         return Response(
+             response=json.dumps(
+                 {"message": "Failed to Update"}
+             ),
+             status=500,
+             mimetype="application/json"
+         )
+@app.route("/users/<id>",methods=["DELETE"])
+def delete(id):
+    try:
+        dbResponse=db.users.delete_one({"_id":ObjectId(id)})
+        if dbResponse.deleted_count==1 :
+            return Response(
+                response=json.dumps({
+                    "message": f"Sucessfully Deleted {id}"
+                }),
                 status=200,
                 mimetype="application/json"
             )
         else :
             return Response(
-                response=json.dumps(
-                    {"message": "Nothing Updated", }),
-                status=200,
+                response=json.dumps({
+                    "message": "Nothing to Delete"
+                }),
+                status=400,
                 mimetype="application/json"
             )
-    except Exception as ex:
-        print(ex)
-        return Response(
-            response=json.dumps(
-                {"message": "Cannot Update Users", }),
-            status=500,
-            mimetype="application/json"
-        )
-@app.route("/users/<id>",methods =["DELETE"])
-def delete(id):
-    try :
-        dbResponse = db.users.delete_one({"_id":ObjectId(id)})
-        if dbResponse.deleted_count == 1 :
 
-            return Response(
-            response=json.dumps(
-                {"message": "User Deleted Successfully", "id":f"{id}"}),
+    except Exception as exp:
+        print(exp)
+    return Response(
+        response=json.dumps({
+            "message":"Failed to Delete"
+        }),
+        status=500,
+        mimetype="application/json"
+    )
+@app.route("/user/<id>",methods=["GET"])
+def getone(id):
+    try:
+        dbResponse =db.users.find_one({"_id":ObjectId(id)})
+        return Response(
+            response=json.dumps({
+                "data": f"{dbResponse}"
+            }),
             status=200,
             mimetype="application/json"
         )
+
+    except Exception as exp:
+        print(exp)
         return Response(
-            response=json.dumps(
-                {"message": "User not Found", "id": f"{id}"}),
-            status=200,
-            mimetype="application/json"
-        )
-    except Exception as ex:
-        print(ex)
-        return Response(
-            response=json.dumps(
-                {"message":"Cannot Delete user"}
-            ),
+            response=json.dumps({
+                "message": "Failed fetch"
+            }),
             status=500,
             mimetype="application/json"
         )
 
 
-# **********
-@app.route("/signup", methods=["POST"])
+# ********************JWT*******************************
+
+@app.route("/users/signup",methods=["POST"])
 def signup():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    try:
+        data = request.get_json()
+        email = data.get("email")
+        password = data.get("password")
 
-    if username and password:
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        user = {"username": username, "password": hashed_password}
-        db.users.insert_one(user)
-        return jsonify({"message": "User registered successfully"}), 201
-    else:
-        return jsonify({"message": "Username and password are required"}), 400
+        if email and password :
+            check_email= db.signup.find_one({"email":email})
+            if check_email:
+                return jsonify({"message": "email already Exists"}), 401
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt())
+            user={"email":email , "password":hashed_password}
+            dbResponse = db.signup.insert_one(user)
+            return jsonify({"message":"user created Successful ","id":f"{dbResponse.inserted_id}"}),201
+        else :
+            return jsonify({"message": "user not created"}), 500
 
-# Login API
-@app.route("/login", methods=["POST"])
+    except Exception as exp:
+        print(exp)
+        return Response(
+            response=json.dumps({
+                "message": "Failed to add user"
+            }),
+            status=500,
+            mimetype="application/json"
+        )
+
+@app.route("/users/login",methods=["POST"])
 def login():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    try:
+        data= request.get_json()
+        email = data.get("email")
+        password = data.get("password")
 
-    user = db.users.find_one({"username": username})
-    if user and bcrypt.checkpw(password.encode("utf-8"), user["password"]):
-        access_token_expires = timedelta(minutes=15)  # Set access token expiry time
-        refresh_token_expires = timedelta(days=30)
-        access_token = create_access_token(identity=username, expires_delta=access_token_expires)
-        refresh_token = create_access_token(identity=username, expires_delta=refresh_token_expires)
-        db.refresh_token.insert_one({"username": username, "token": refresh_token})
-        return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
-    else:
-        return jsonify({"message": "Invalid credentials"}), 401
+        user = db.signup.find_one({"email":email})
+        if user and bcrypt.hashpw(password.encode("utf-8"),user["password"]):
+            access_token_expiry = timedelta(minutes=15)
+            refresh_token_expiry= timedelta(days=7)
+            access_token=create_access_token(identity="username",expires_delta=access_token_expiry)
+            refresh_token = create_access_token(identity="username", expires_delta=refresh_token_expiry)
+            return jsonify({"data":{"access_token":f"{access_token}",}}) , 200
+        else:
+            return jsonify({"message":"Invalid Credentials"})
+
+    except Exception as exp:
+        print(exp)
+        return Response(
+            response=json.dumps({
+                "message": "Failed Login In"
+            }),
+            status=500,
+            mimetype="application/json")
+
+@app.route("/refreshtoken",methods=["GET"])
+@jwt_required()
+def refresh_token():
+    users = db.signup
+    user = get_jwt_identity()
+    expiry = timedelta(days=7)
+    refresh_token = create_access_token(user,expires_delta=expiry)
+    db.refreshToken.insert_one({"email": f"{users.email}", "refresh_token": refresh_token})
+    return jsonify({"refresh_token":f"{refresh_token}"}),200
 
 
-@app.route("/protected", methods=["GET"])
+@app.route("/home",methods=["GET"])
 @jwt_required()
 def protected():
-    current_user = get_jwt_identity()
-    return jsonify({"message": f"Welcome, {current_user}!"}), 200
+    check = get_jwt_identity()
+    if check :
+        return jsonify({}),200
 
-
-@app.route("/refresh", methods=["POST"])
-@jwt_required()
-def refresh():
-    current_user = get_jwt_identity()
-    new_access_token = create_access_token(identity=current_user)
-    return jsonify({"refresh_token": new_access_token}), 200
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(port=8000, debug=True)
